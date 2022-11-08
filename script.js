ac1d0cb0bea1e4e4ec5ab331286f417ab229c41a
Almas Barzhaxynov <almas@oomzie.com>
2022-11-07 19:24:01 -0500

    updated javascript to script file

---
+++ script.js
@@ -0,0 +1,19 @@
+function reveal() {
+  window.addEventListener('scroll', (e) => {
+    var reveal = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveal.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveal[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveal[i].classList.add("active");
+      } else {
+        reveal[i].classList.remove("active");
+      }
+    }
+  });
+}
+
+reveal();
